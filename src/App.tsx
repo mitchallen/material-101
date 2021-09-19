59b74a6bc82d41172a6fee802b96454ee520d8d5
Mitch Allen <github@mitchallen.com>
2021-09-19 06:36:17 -0400

    first pass at working example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import { Button, Card, CardActions, CardContent, FormControl, TextField, Tooltip, Typography } from '@mui/material';
+import { SmartToy } from '@mui/icons-material';
+import { AppStyles } from './AppStyles';
 
 function App() {
+
+  const [userName, setUserName] = useState('');
+
+  const handleUserNameChange = useCallback((event) => {
+    setUserName(event.target.value);
+  }, []);
+
+  const handleClickHello = useCallback((event) => {
+    alert(`Hello ${userName}!`)
+  }, [userName]);
+
+  const handleClickYo = useCallback((event) => {
+    alert(`Yo ${userName}!`)
+  }, [userName]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card style={AppStyles.cardStyle}>
+      <CardContent>
+        <SmartToy style={AppStyles.logoStyle} fontSize="large" />
+        <Typography gutterBottom variant="h5" component="h2">
+          Material 101
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <FormControl style={{ width: '50%' }} >
+          <TextField
+            id="userName"
+            name='userName'
+            label="Your name"
+            value={userName}
+            onChange={handleUserNameChange}
+            helperText='Enter your name'
+          />
+        </FormControl>
+      </CardContent>
+      <CardActions>
+        <Tooltip title="Click to say hello">
+          <span>
+            <Button color='primary'
+              disabled={userName.length === 0}
+              variant="outlined"
+              onClick={handleClickHello}
+              key={1}>
+              Say Hello
+            </Button>
+          </span>
+        </Tooltip>
+        <Tooltip title="Click to say yo">
+          <span>
+            <Button color='secondary'
+              disabled={userName.length === 0}
+              variant="outlined"
+              onClick={handleClickYo}
+              key={1}>
+              Say Yo
+            </Button>
+          </span>
+        </Tooltip>
+      </CardActions>
+    </Card>
   );
 }
 
---
+++ src/AppStyles.tsx
@@ -0,0 +1,16 @@
+export const AppStyles: any = {
+    cardStyle: {
+        width: '400px',
+        margin: 'auto',
+        marginTop: 20,
+        textAlign: 'center',
+        display: 'block',
+    },
+    logoStyle: { 
+        marginTop: '20px', 
+        marginBottom: '20px', 
+        padding: '10px', 
+        border: '1px solid black', 
+        borderRadius: '50%' 
+    }
+}
